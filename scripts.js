f6263059849ba4b4c2086bd48ac3305808aadd1f
Ivan <156574578+ivanlyubenov12@users.noreply.github.com>
2024-10-02 18:00:32 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL = 'YOUR_SCRIPT_WEB_APP_URL';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyb6cuzzmX1K6iRqJvHKw5WOYpPot1JYbAJgGBdPcSKhmCWIx8rz-xPH1czz8uLSf6x/exec';
 
 // Fetch homework and display it in sticky notes
 function fetchHomework() {
