afe08cb478d47b05508302770778e6913a96a96b
Ivan <156574578+ivanlyubenov12@users.noreply.github.com>
2024-10-02 18:43:04 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyb6cuzzmX1K6iRqJvHKw5WOYpPot1JYbAJgGBdPcSKhmCWIx8rz-xPH1czz8uLSf6x/exec'; // Replace with your actual URL
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzebV2dbAsISdNJKTus6F-gsO_lixSPPp8M7Ad93IespMIrKeAYkFAeI_thzTCVVcNK/exec'; // Replace with your actual URL
 
 function fetchHomework() {
   fetch(`${SCRIPT_URL}?action=getHomework`)
