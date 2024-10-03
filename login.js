0f13bc89c05d81c951f34122f5df8341d5052947
Ivan <156574578+ivanlyubenov12@users.noreply.github.com>
2024-10-03 20:22:27 +0300

    Create login.js

---
+++ login.js
@@ -0,0 +1,10 @@
+import { loginAdmin } from './firebase.js';
+
+document.getElementById('login-form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    await loginAdmin(email, password);
+});
